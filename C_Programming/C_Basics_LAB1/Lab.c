6b7de146f5e4b2d2c191d8fac9dfe0d5842c2378
hassan <Hassan@abd_Eltawab.com>
2021-06-14 12:26:05 +0200

    uploading the first lab

---
+++ C_Programming/C_Basics_LAB1/Lab.c
@@ -0,0 +1,18 @@
+/*
+ * Lab.c
+ *
+ *  Created on: 14 Jun 2021
+ *      Author: hassan
+ */
+#include<stdio.h>
+void main(){
+	unsigned char i ,j;
+	for(i=0;i<=9;i++){
+		for(j=i;j<=9;j++){
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+}
+
+
