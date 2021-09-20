c6aceda21e60291b2715673ea4a02f8b0fab599e
hassan <Hassan@abd_Eltawab.com>
2021-09-20 23:27:06 +0200

    GPIO DRIVER

--- Unit7/Lesson1_Assignment/Atmega32_Led_Sequance/main.c
+++ Unit7/Lesson1_Assignment/Atmega32_Led_Sequance/main.c
@@ -12,15 +12,15 @@
  *      Author: hassan
  */
 #include<util/delay.h>
-#include<avr/io.h>
-/*
-#define SREG   *(volatile unsigned char*)(0x3F)
-#define GICR   *(volatile unsigned char*)(0x3B)
-#define MCUCSR *(volatile unsigned char*)(0x34)
-#define MCUCR  *(volatile unsigned char*)(0x35)
-#define PORTD  *(volatile unsigned char*)(0x12)
-#define DDRD   *(volatile unsigned char*)(0x11)
-#define PIND   *(volatile unsigned char*)(0x10)*/
+//#include<avr/io.h>
+
+#define SREG   *(volatile unsigned char*)(0x5F)
+#define GICR   *(volatile unsigned char*)(0x5B)
+#define MCUCSR *(volatile unsigned char*)(0x54)
+#define MCUCR  *(volatile unsigned char*)(0x55)
+#define PORTD  *(volatile unsigned char*)(0x32)
+#define DDRD   *(volatile unsigned char*)(0x31)
+#define PIND   *(volatile unsigned char*)(0x30)
 void PORTD_init(){
 	/*PORTD 4,5,6,7 as O/P*/
 	DDRD |=(0xF<<4);
---
+++ Unit7/Lesson3/Drivers/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ D:/Online_Emb_Diploma/GitHub/Online_Embedded_Diploma/Unit7/Lesson4/Drivers/Stm32f103c6_Drivers/inc/Stm32_F103C6_gpio_driver.h \
+ D:/Online_Emb_Diploma/GitHub/Online_Embedded_Diploma/Unit7/Lesson4/Drivers/Stm32f103c6_Drivers/inc/stm32f103x6.h
+
+D:/Online_Emb_Diploma/GitHub/Online_Embedded_Diploma/Unit7/Lesson4/Drivers/Stm32f103c6_Drivers/inc/Stm32_F103C6_gpio_driver.h:
+
+D:/Online_Emb_Diploma/GitHub/Online_Embedded_Diploma/Unit7/Lesson4/Drivers/Stm32f103c6_Drivers/inc/stm32f103x6.h:
